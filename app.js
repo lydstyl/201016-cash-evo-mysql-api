d9d3d20590d35b4b841f406cc1147f0ac0784353
Gabriel Brun <lydstyl@gmail.com>
2020-10-16 18:02:12 +0200

    has been blocked by CORS policy

--- app.js
+++ app.js
@@ -10,6 +10,15 @@ const userRoutes = require('./routes/user')
 
 const app = express()
 
+app.all('*', (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+
+  next()
+})
+
+// app.use(express.static(environmentRoot + '/public'))
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api/v1', userRoutes)
