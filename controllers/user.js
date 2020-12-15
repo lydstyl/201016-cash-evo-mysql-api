b3b70d0c5ee5e02d5e72a893adc268d819fc7d0c
Gabriel Brun <lydstyl@gmail.com>
2020-12-15 18:38:51 +0100

    bcrypt signup & login

--- controllers/user.js
+++ controllers/user.js
@@ -1,42 +1,35 @@
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 const User = require('../models/user')
-
-// @desc    Login user
-// @route   POST /api/v1/login
-// @access  Public
-exports.postLogin = (req, res, next) => {
-  const { email, password } = req.body
-
-  // todo unhash password
-
-  if (password === process.env.TEMPORARY_PASSWORD) {
-    const token = jwt.sign({
-      data: email
-    }, process.env.JWT_SECRET, { expiresIn: '1h' })
-
-    res.status(200).json({ success: true, msg: 'User loged in !', token })
-  } else {
-    res.status(500).json({ success: false, msg: 'Error in postLogin controller' })
-  }
-}
 
 // @desc    Create user === sign up
 // @route   POST /api/v1/users
 // @access  Public
 exports.postUsers = async (req, res, next) => {
   try {
-    const { email, password } = req.body
+    let { email, password } = req.body
 
-    // todo hash password
+    // hash password
 
-    const result = await User.create({ email, password })
+    const saltRounds = 10
+    const myPlaintextPassword = password
+    bcrypt.hash(myPlaintextPassword, saltRounds, async function (err, hash) {
+      // Store hash in your password DB.
+      if (err) {
+        console.log('🚀 ~ bcrypt.hash ~ err', err)
+      } else {
+        password = hash
 
-    const user = result.dataValues
+        const result = await User.create({ email, password })
 
-    res
-      .status(200)
-      .json({ success: true, msg: 'User created', data: user })
+        const user = result.dataValues
+
+        res
+          .status(200)
+          .json({ success: true, msg: 'User created', data: user })
+      }
+    })
   } catch (error) {
     console.log('🚀 ~ exports.postUsers= ~ error', error)
 
@@ -95,5 +88,35 @@ exports.deleteUser = async (req, res, next) => {
     console.log('🚀 ~ exports.postUsers= ~ error', error)
 
     res.status(500).json({ success: false, error })
+  }
+}
+
+// @desc    Login user
+// @route   POST /api/v1/login
+// @access  Public
+exports.postLogin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body
+
+    // unhash password
+    const user = await User.findOne({ where: { email } })
+
+    bcrypt.compare(password, user.password, function (err, result) {
+      if (err) {
+        console.log('🚀 ~ err', err)
+      } else {
+        if (result) {
+          const token = jwt.sign({
+            data: email
+          }, process.env.JWT_SECRET, { expiresIn: '1h' })
+
+          res.status(200).json({ success: true, msg: 'User loged in !', token })
+        } else {
+          res.status(500).json({ success: false, msg: 'Error in postLogin controller' })
+        }
+      }
+    })
+  } catch (error) {
+    console.log('🚀 ~ exports.postLogin= ~ error', error)
   }
 }
