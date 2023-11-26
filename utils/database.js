97c1dbde5464d79fb394b10a48b10e9858225428
Gabriel Brun <lydstyl@gmail.com>
2023-11-26 13:50:48 +0100

    Change the app so it works with sqlite in local because I can't access Heroku anymore.

--- utils/database.js
+++ utils/database.js
@@ -1,13 +1,20 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    dialect: 'mysql',
-    host: process.env.DB_HOST
-  }
-)
+// // with old Heroku database
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     dialect: 'mysql',
+//     host: process.env.DB_HOST
+//   }
+// )
+
+// // with Sqlite3
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "./database.sqlite",
+})
 
 module.exports = sequelize
